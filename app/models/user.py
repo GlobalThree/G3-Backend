741abdb1007fff78d12af7d09fd9fbcf68c4b1dd
jutole <jutole@jutoleui-Macmini.local>
2025-02-08 19:34:54 +0900

    유저 모델 생성

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,19 @@
 from app.db.base import Base
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime, timezone
 
 
 class User(Base):
-    _tablename__ = "user_account"
+    __tablename__ = "user_account"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
+    updated_at = Column(
+        DateTime,
+        default=datetime.now(timezone.utc),
+        onupdate=datetime.now(timezone.utc),
+        nullable=False,
+    )
