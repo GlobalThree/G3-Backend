e4abee1a26a8b3b5feb7a76e18784a83a83abe0f
jutole <jutole@jutoleui-Macmini.local>
2025-02-08 22:33:12 +0900

    user crud 완료

--- app/crud/user.py
+++ app/crud/user.py
@@ -2,10 +2,38 @@ from sqlalchemy.orm import Session
 from app.schemas import user as user_schema
 from app.models.user import User
 from fastapi import HTTPException
+from app.schemas.user import UserResponse
 
 
 def get_user_by_email(email: str, db: Session):
     return db.query(User).filter(User.email == email).first()
+
+
+def get_users(db: Session) -> list[UserResponse]:
+    try:
+        users = db.query(User).all()
+    except Exception:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+    user_responses = [
+        UserResponse(
+            email=user.email,
+            name=user.name,
+            id=user.id,
+        )
+        for user in users
+    ]
+    return user_responses
+
+
+def get_user(id: int, db: Session) -> UserResponse:
+    try:
+        user: User | None = db.query(User).filter(User.id == id).first()
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        return UserResponse(email=user.email, id=user.id, name=user.name)
+    except Exception:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
 def create_user(user: user_schema.UserCreate, db: Session):
